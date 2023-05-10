296ed37364d700bc9d09c0b9a420f01023ca4201
Eudes Morais <eudes.morais@gmail.com>
2023-05-09 21:29:06 -0300

    Get game by id

--- src/main/java/com/eudes/dslistbackend/services/GameService.java
+++ src/main/java/com/eudes/dslistbackend/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.eudes.dslistbackend.dto.GameDTO;
 import com.eudes.dslistbackend.dto.GameMinDTO;
 import com.eudes.dslistbackend.entities.Game;
 import com.eudes.dslistbackend.repositories.GameRepository;
@@ -15,6 +17,16 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+
+        GameDTO dto = new GameDTO(result);
+
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
 
@@ -22,4 +34,5 @@ public class GameService {
 
         return dto;
     }
+
 }
